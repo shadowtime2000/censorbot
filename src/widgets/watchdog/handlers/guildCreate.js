88f2d00f455c4997dee3451314b7c050dcf8a535
shadowtime2000 <66655515+shadowtime2000@users.noreply.github.com>
2020-09-08 20:27:36 -0700

    Changes

--- src/widgets/watchdog/handlers/guildCreate.js
+++ src/widgets/watchdog/handlers/guildCreate.js
@@ -1,7 +1,6 @@
 module.exports = (guild) => {
     if (guild.available) {
         const stamp = new Date(guild.joinedTimestamp).toISOString();
-        console.log(stamp)
         const logMessage = [
             `${stamp.substring(0, 10)} ${stamp.substring(11, 19)}`,
             guild.name,
