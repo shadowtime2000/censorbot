091eb46d98f3a1091850832e8fea4e0594d2113d
shadowtime2000 <66655515+shadowtime2000@users.noreply.github.com>
2020-09-08 20:36:25 -0700

    Attempt at Fixing Repeated Self Mentioning Delete Loop

--- src/widgets/messageReader/handlers/message.js
+++ src/widgets/messageReader/handlers/message.js
@@ -1,14 +1,18 @@
 const wordDB = require("../../../../wordDB");
 
 module.exports = (message) => {
+    if (!message.guild) return;
     let words = message.content.split(/[\b\n\s(?!$.)]/);
     words = words.filter(v => v !== "");
+
+    if (message.member.hasPermission("ADMINISTRATOR")) return;
+    if(message.author.id === message.client.user.id) return;
 
     for (const word in words) {
         if (wordDB.has(`${message.guild.id}${words[word]}`) && !message.member.hasPermission("ADMINISTRATOR")) {
             message.delete({ reason: "Contains banned word" })
                 .then(() => message.reply("Your message has been deleted for containing a banned word"))
-                .catch(() => message.channel.send("I require message delete permissions to function"))
+                .catch((error) => { console.error(error); message.channel.send((error === undefined ? "Something went wrong" : error.toString()));})
         }
     }
 }
